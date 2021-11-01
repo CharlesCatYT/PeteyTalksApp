55e6a7d23059a1e45a1b52059c8eb49cdb124def
Sonic Pro YT <62410083+SonicProYT@users.noreply.github.com>
2021-11-01 18:22:25 -0400

    Updated by CrossUI No-Code App Builder

--- App/js/RichEditor.js
+++ App/js/RichEditor.js
@@ -19,7 +19,19 @@ xui.Class('App.RichEditor', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.RichEditor")
+                .setHost(host,"xui_ui_richeditor2")
+                .setLeft("0.7619047619047619em")
+                .setTop("0.7619047619047619em")
+                .setHeight("26em")
+                .setLabelSize("2em")
+                .setLabelPos("top")
+                .setLabelCaption("RichEditor")
+                .setLabelHAlign("left")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
